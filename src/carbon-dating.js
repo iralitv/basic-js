14202fc08a079b600e983b52ddedb76aded39bac
Iralitv <ira.litvinchuk@mifort.org>
2020-02-19 06:00:50 +0300

    feat: resolve float with 2 dots parsing

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,12 +2,13 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample( sampleActivity ) {
+    const parseSampleActivity = parseFloat(sampleActivity);
    if ( typeof sampleActivity !== 'string' ||
-       +sampleActivity >= MODERN_ACTIVITY ||
-       +sampleActivity <= 0 ||
-       +sampleActivity !== +sampleActivity) return false;
+       parseSampleActivity >= MODERN_ACTIVITY ||
+       parseSampleActivity <= 0 ||
+       parseSampleActivity !== parseSampleActivity) return false;
 
-    const sampleRatio = MODERN_ACTIVITY / sampleActivity;
+    const sampleRatio = MODERN_ACTIVITY / parseSampleActivity;
     const rateConstant = 0.693 / HALF_LIFE_PERIOD;
     const carbonDate = Math.log(sampleRatio) / rateConstant;
 
