b6725193eac49e30cfe49f69ea73b91bb793f042
Iralitv <ira.litvinchuk@mifort.org>
2020-02-19 05:53:36 +0300

    feat: resolve all test except 2 dots in float

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,15 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample( sampleActivity ) {
+   if ( typeof sampleActivity !== 'string' ||
+       +sampleActivity >= MODERN_ACTIVITY ||
+       +sampleActivity <= 0 ||
+       +sampleActivity !== +sampleActivity) return false;
+
+    const sampleRatio = MODERN_ACTIVITY / sampleActivity;
+    const rateConstant = 0.693 / HALF_LIFE_PERIOD;
+    const carbonDate = Math.log(sampleRatio) / rateConstant;
+
+    return Math.floor(carbonDate) + 1;
 };
