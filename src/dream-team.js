eea43af3109c4a8a8832008379161b009815b827
Iralitv <ira.litvinchuk@mifort.org>
2020-02-20 06:56:59 +0300

    feat: resolve dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if ( !Array.isArray(members) ) return false;
+
+  const teamTitle = members.filter(Boolean).map(item => {
+    if(typeof item === 'string') {
+      const itemTrim = item.trim();
+      return itemTrim[0].toUpperCase()
+    }
+  });
+
+  return [...teamTitle].sort().join('')
 };
