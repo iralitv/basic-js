a8fa46e4fe8a9fdd25fc3f1413743a4461ebfbe1
Iralitv <ira.litvinchuk@mifort.org>
2020-02-20 07:52:16 +0300

    feat: resolve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,19 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+    const {
+        repeatTimes = 1,
+        separator = '+',
+        addition = '',
+        additionRepeatTimes = 0,
+        additionSeparator = ''
+    } = options;
+
+    const addExprWithoutLastSeparator =
+        ( addition + additionSeparator )
+            .repeat(additionRepeatTimes)
+            .slice(0, -additionSeparator.length);
+
+    const addExpr = ( additionRepeatTimes < 2 ) ? addition : addExprWithoutLastSeparator;
+
+    return ( str + addExpr + separator ).repeat(repeatTimes).slice( 0, -separator.length )
 };
   
