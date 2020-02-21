ab45674df35663d0ebaa7e8a7c8adf95632ad547
Iralitv <ira.litvinchuk@mifort.org>
2020-02-21 19:29:22 +0300

    feat: array transform without properly works test

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,15 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform( arr ) {
+    arr.forEach((item, index, array) => {
+        if (item === '--discard-next') {
+            array.splice(index, 2)
+        } else if (item === '--discard-prev') {
+            array.splice(index - 1, 2)
+        } else if (item === '--double-next') {
+            array.splice(index, 1, array[index + 1])
+        } else if (item === '--double-prev') {
+            array.splice(index, 1, array[index - 1])
+        }
+    })
+
+    return arr
 };
