77c86001aa50536440d6999711a15fed9d04646b
Iralitv <ira.litvinchuk@mifort.org>
2020-02-25 07:02:43 +0300

    feat: resolve hanoi-tower with recursion

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,5 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let calculateTurns = (disksNumber) => disksNumber === 1 ? 1 : 2 * calculateTurns(disksNumber - 1) + 1
+
+    return { turns: calculateTurns(disksNumber), seconds: calculateTurns(disksNumber) / (turnsSpeed / 3600) }
 }
