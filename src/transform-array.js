4649a6466869cd7db11cdf8be3a0f45605a41e98
Iralitv <ira.litvinchuk@mifort.org>
2020-02-25 11:21:00 +0300

    feat: resolve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,15 +1,35 @@
 module.exports = function transform( arr ) {
-    arr.forEach((item, index, array) => {
-        if (item === '--discard-next') {
-            array.splice(index, 2)
-        } else if (item === '--discard-prev') {
-            array.splice(index - 1, 2)
-        } else if (item === '--double-next') {
-            array.splice(index, 1, array[index + 1])
-        } else if (item === '--double-prev') {
-            array.splice(index, 1, array[index - 1])
-        }
-    })
+    if (!Array.isArray(arr)) throw Error('it\'s not an array');
 
-    return arr
+    const transformArray = [];
+    const transformControls = [
+        '--double-prev',
+        '--double-next',
+        '--discard-next',
+        '--discard-prev'
+    ];
+
+    for (let i = 0; i < arr.length; i++) {
+        if(transformControls.includes(arr[i])) {
+            switch(arr[i]) {
+                case '--double-next':
+                    if (i + 1 < arr.length) transformArray.push(arr[i + 1])
+                    break;
+                case '--discard-prev':
+                    transformArray.pop();
+                    break;
+                case '--discard-next':
+                    if (i + 2 < arr.length) i++;
+                    break;
+                case '--double-prev':
+                    if (i - 1 > 0) transformArray.push(arr[i - 1]);
+                    break;
+                default: break;
+            }
+        } else {
+            transformArray.push(arr[i]);
+        }
+    }
+
+    return transformArray;
 };
