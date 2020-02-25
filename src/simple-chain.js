1c459332826c4a70a6213bb6649b5133be4b1747
Iralitv <ira.litvinchuk@mifort.org>
2020-02-25 09:04:50 +0300

    feat: resolve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,30 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let addingElem = typeof value === 'undefined' ? '' : value;
+    this.chain.push(`( ${addingElem} )`);
+    return this
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position !== 'number' || position > this.chain.length || position <= 0 ) {
+      this.chain = [];
+      throw 'wrong position number';
+    }
+
+    this.chain.splice(position - 1,1);
+    return this
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let finishChain = this.chain.join('~~');
+    this.chain = [];
+    return finishChain;
   }
 };
 
