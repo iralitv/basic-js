a3e0a9fdcfa49b1cc9f4dbae57107fc2db96b031
Iralitv <ira.litvinchuk@mifort.org>
2020-02-25 07:19:02 +0300

    feat: resolve recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,7 +1,7 @@
 module.exports = class DepthCalculator {
-    calculateDepth(arr) {
-        return 1 + (arr instanceof Array ?
-          arr.reduce((max, item) => Math.max(max, this.calculateDepth(item)), 0) :
-          -1)
+    calculateDepth(arr, depth = 1) {
+        return arr.some( item => Array.isArray(item) ) ?
+          this.calculateDepth(arr.flat(), depth += 1) :
+          depth
     }
 };
