b3d0278b35f4b9611d29fc4a2e4c949dd364c22a
Iralitv <ira.litvinchuk@mifort.org>
2020-03-16 21:31:24 +0300

    feat: resolve what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,16 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+const FALL = ['Sep', 'Oct', 'Nov'];
+const WINTER = ['Dec', 'Jan', 'Feb'];
+const SPRING = ['Mar', 'Apr', 'May'];
+const SUMMER = ['Jun', 'Jul', 'Aug'];
+
+module.exports = function getSeason(date) {
+  if (typeof date === 'undefined' ) return 'Unable to determine the time of year!';
+  if(!(date instanceof Date)) throw new Error();
+
+  const season = date.toDateString().split(' ')[1];
+
+  if(FALL.includes(season)) return 'fall';
+  if(WINTER.includes(season)) return 'winter';
+  if(SPRING.includes(season)) return 'spring';
+  if(SUMMER.includes(season)) return 'summer';
 };
